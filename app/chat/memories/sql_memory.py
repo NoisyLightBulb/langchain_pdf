16f920539988d809fbfce70b08d9af0a61962489
Florian Hinterleitner <florian.hinterleitner@gmail.com>
2024-05-30 18:12:18 +0200

    sql history class created

---
+++ app/chat/memories/sql_memory.py
@@ -0,0 +1,23 @@
+from langchain_core.messages import BaseMessage
+from pydantic import BaseModel
+from langchain.memory import ConversationBufferMemory
+from langchain.schema import BaseChatMessageHistory
+
+from app.web.api import get_messages_by_conversation_id, add_message_to_conversation
+
+class SqlMessageHistory(BaseChatMessageHistory, BaseModel):
+    converstation_id: str
+
+    @property
+    def messages(self):
+        return get_messages_by_conversation_id(self.converstation_id)
+
+    def add_message(self, message):
+        return add_message_to_conversation(
+            conversation_id = self.converstation_id,
+            role = message.type,
+            content = message.content
+        )
+
+    def clear(self):
+        pass
